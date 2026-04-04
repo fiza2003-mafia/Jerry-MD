a9f7b35b1e0934d90116aa299efdc6278491aee5
fiza2003-mafia <fk9451712@gmail.com>
2026-04-04 12:12:21 +0500

    Update newsletter information in ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -41,8 +41,8 @@ async (conn, mek, m, { from, quoted, sender, reply }) => {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363348739987203@newsletter',
-                    newsletterName: "𝐀𝐫𝐬𝐥𝐚𝐧_𝐌𝐃",
+                    newsletterJid: '153850577215541@newsletter',
+                    newsletterName: "Jerry_𝐌𝐃",
                     serverMessageId: 143
                 }
             }
